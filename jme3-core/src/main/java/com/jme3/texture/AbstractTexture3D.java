6183132125d404b81d74c7f6b9cc0ea8e8b5d3d3
dingenad <jPXjEd{9GP>
2018-09-05 14:25:08 +0200

    - Write code once.
    Refactored code.

---
+++ jme3-core/src/main/java/com/jme3/texture/AbstractTexture3D.java
@@ -0,0 +1,86 @@
+package com.jme3.texture;
+
+public abstract class AbstractTexture3D extends Texture{
+
+    protected WrapMode wrapS = WrapMode.EdgeClamp;
+    protected WrapMode wrapT = WrapMode.EdgeClamp;
+    protected WrapMode wrapR = WrapMode.EdgeClamp;
+
+    @Override
+    public Texture createSimpleClone(Texture rVal) {
+        rVal.setWrap(WrapAxis.S, wrapS);
+        rVal.setWrap(WrapAxis.T, wrapT);
+        rVal.setWrap(WrapAxis.R, wrapR);
+        return super.createSimpleClone(rVal);
+    }
+
+    /**
+     * <code>setWrap</code> sets the wrap mode of this texture for a
+     * particular axis.
+     *
+     * @param axis
+     *            the texture axis to define a wrapmode on.
+     * @param mode
+     *            the wrap mode for the given axis of the texture.
+     * @throws IllegalArgumentException
+     *             if axis or mode are null
+     */
+    public void setWrap(WrapAxis axis, WrapMode mode) {
+        if (mode == null) {
+            throw new IllegalArgumentException("mode can not be null.");
+        } else if (axis == null) {
+            throw new IllegalArgumentException("axis can not be null.");
+        }
+        switch (axis) {
+            case S:
+                this.wrapS = mode;
+                break;
+            case T:
+                this.wrapT = mode;
+                break;
+            case R:
+                this.wrapR = mode;
+                break;
+        }
+    }
+
+    /**
+     * <code>setWrap</code> sets the wrap mode of this texture for all axis.
+     *
+     * @param mode
+     *            the wrap mode for the given axis of the texture.
+     * @throws IllegalArgumentException
+     *             if mode is null
+     */
+    public void setWrap(WrapMode mode) {
+        if (mode == null) {
+            throw new IllegalArgumentException("mode can not be null.");
+        }
+        this.wrapS = mode;
+        this.wrapT = mode;
+        this.wrapR = mode;
+    }
+
+    /**
+     * <code>getWrap</code> returns the wrap mode for a given coordinate axis
+     * on this texture.
+     *
+     * @param axis
+     *            the axis to return for
+     * @return the wrap mode of the texture.
+     * @throws IllegalArgumentException
+     *             if axis is null
+     */
+    public WrapMode getWrap(WrapAxis axis) {
+        switch (axis) {
+            case S:
+                return wrapS;
+            case T:
+                return wrapT;
+            case R:
+                return wrapR;
+        }
+        throw new IllegalArgumentException("invalid WrapAxis: " + axis);
+    }
+
+}
